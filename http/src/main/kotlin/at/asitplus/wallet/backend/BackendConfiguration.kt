c9f44dc78cba55d6b806a8dc5a337b9f751d2f1f
Lukas Alber <lukas.alber@iaik.tugraz.at>
2021-08-24 10:48:41 +0200

    refactor to class and null saver

--- http/src/main/kotlin/at/asitplus/wallet/backend/BackendConfiguration.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/BackendConfiguration.kt
@@ -36,7 +36,7 @@ class BackendConfiguration {
         return IdentifierRegistry(identifierRepository)
     }
 
-    @Bean
+    // Fallback code
     fun issuerCredentialDataProvider() = object : IssuerCredentialDataProvider {
         private val defaultLifetime = configurationProperties.credentialLifetime
 
@@ -66,107 +66,7 @@ class BackendConfiguration {
     }
 
     @Bean
-    fun issuerCredentialRandomDataProvider() = object : IssuerCredentialDataProvider {
-
-        private val desiredPictureSize = "256"
-        private val defaultLifetime = configurationProperties.credentialLifetime
-        private val client = OkHttpClient()
-
-        fun getResponse(url: String) = client.newCall(Request.Builder().url(url).build()).execute()
-
-        // random Parameters
-        private val gender = listOf("male", "female").random()
-        private val gen_ = when (gender) {
-            "male" -> "m"
-            else -> "f"
-        }
-        private val name = object {
-            private val nameArray =
-                getResponse("https://www.behindthename.com/api/random.json?usage=ger&gender=${gen_}&key=lu244794741")
-                    .takeIf { response2 -> response2.isSuccessful }
-                    ?.let {
-                        Json.parseToJsonElement(it.body()!!.string()).jsonObject["names"]!!.jsonArray
-                    }
-
-            val firstName = nameArray!![1].jsonPrimitive.content
-            val lastName = nameArray!![0].jsonPrimitive.content
-        }
-
-        private var address = object {
-
-            private val string = client.newCall(
-                Request.Builder()
-                    .url("https://randommer.io/random-address")
-                    .post(
-                        FormBody.Builder()
-                            .add("number", "1")
-                            .add("culture", "de_AT")
-                            .build()
-                    )
-                    .build()
-            ).execute()
-                .takeIf { it.isSuccessful }
-                ?.let { response ->
-                    val body = Json.parseToJsonElement(response.body()!!.string()).jsonArray
-                    body[0].jsonPrimitive.content
-                }
-
-            val school = string?.split(",")?.toMutableList()?.also{ it.removeAt(1)}
-            ?.toList()?.joinToString(",") ?: "Breitenseer Straße 13, 1140, Wien, Austria"
-
-            val county = string?.split(",")?.get(3) ?: "Wien"
-
-            val zip = string?.split(",")?.get(2) ?: "1010"
-        }
-
-
-        private var encodedPhoto = Request.Builder()
-            .url("https://api.generated.photos/api/v1/faces?age=child&page=1&per_page=1&gender=${gender}")
-            .addHeader("Authorization", "API-Key L6VjhxOfJUKWu3xrLnwVIg") //L6VjhxOfJUKWu3xrLnwVIg
-            .build().let { request1 ->
-                client.newCall(request1).execute()
-                    .takeIf { response1 -> response1.isSuccessful }
-                    ?.let { response ->
-                        val body = Json.parseToJsonElement(response.body()!!.string()).jsonObject
-                        val faces = body["faces"]!!.jsonArray
-                        val face = faces.get(0).jsonObject
-                        val urls = face.get("urls")!!.jsonArray
-                        val picture = urls.find { url -> url.jsonObject.containsKey(desiredPictureSize) }!!.jsonObject
-                        val picUrl = picture.get(desiredPictureSize)!!.jsonPrimitive.content
-
-                        getResponse(picUrl)
-                            .takeIf { response2 -> response2.isSuccessful }
-                            ?.let { response2 ->
-                                val bytes = response2.body()!!.bytes()
-                                Base64Utils.encode(bytes).decodeToString()
-                            }
-                    }
-            } ?: fallbackPhoto
-
-        override fun getClaim(subjectId: String, attribute: String) = when (attribute) {
-            "photo" -> Claim(attribute, encodedPhoto, "image/jpeg", defaultLifetime)
-            "schulname" -> Claim(attribute, "Quarto Testschule", "application/text", defaultLifetime)
-            "schuladresse" -> Claim(
-                attribute,
-                address.school,
-                "application/text",
-                defaultLifetime
-            )
-            "schulkennzahl" -> Claim(attribute, "101010", "application/text", defaultLifetime)
-            "schülerkennzahl" -> Claim(attribute, "00200000/00000004", "application/text", defaultLifetime)
-            "vorname" -> Claim(attribute, name.firstName, "application/text", defaultLifetime)
-            "nachname" -> Claim(attribute, name.lastName, "application/text", defaultLifetime)
-            "titelvor" -> Claim(attribute, "", "application/text", defaultLifetime)
-            "titelnach" -> Claim(attribute, "", "application/text", defaultLifetime)
-            "geburtsdatum" -> Claim(attribute, "1997-12-26", "application/text", defaultLifetime)
-            "gültigBis" -> Claim(attribute, "2021-07-31", "application/text", defaultLifetime)
-            "wohnort" -> Claim(attribute, address.county, "application/text", defaultLifetime)
-            "wohnort-plz" -> Claim(attribute, address.zip, "application/text", defaultLifetime)
-            "klasse" -> Claim(attribute, "3B", "application/text", defaultLifetime)
-            else -> null
-        }
-
-    }
+    fun issuerCredentialRandomDataProvider() = IssuerCredentialRandomDataProvider(configurationProperties.credentialLifetime, fallbackPhoto)
 
     @Bean
     fun issuerCryptoService() = when (configurationProperties.issuerKey.type) {
