1922bffcc2903c35c81853315a21d01e8d5fbcb8
Lukas Alber <lukas.alber@iaik.tugraz.at>
2021-08-25 10:17:26 +0200

    solving IsssuerCredentialRandomDataProviderTest

--- http/src/main/kotlin/at/asitplus/wallet/backend/IssuerCredentialRandomDataProvider.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/IssuerCredentialRandomDataProvider.kt
@@ -11,59 +11,83 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.springframework.util.Base64Utils
 import java.time.Duration
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 
 class IssuerCredentialRandomDataProvider(
     private val defaultLifetime: Duration,
     private val fallbackPhoto: String
 ) : IssuerCredentialDataProvider {
-
     private val desiredPictureSize = "256"
     private val client = OkHttpClient()
-    private val randomGender = listOf("male", "female").random()
 
-    private val name = object {
-        private val nameArray =
-            executeGet("https://www.behindthename.com/api/random.json?usage=ger&gender=${randomGender[0]}&key=lu244794741")
+    inner class PupilAttributes {
+        val randomGender = listOf("male", "female").random()
+        val randomSchool = listOf(
+            "Schiller", "Tesla", "Newton", "Einstein", "Marie Curie", "Rosalind Franklin",
+            "Anne Frank", "Geschwister Scholl"
+        ).random() + " " + listOf(
+            "Realgymnasium", "Volksschule", "Gymnasium",
+            "Mittelschule", "HTL", "HAK", "Hauptschule"
+        ).random()
+        val schoolCode = (1..6).map { "01".random() }.joinToString("") // e.g. 101010
+        val pupilIdNumber = (1..2)
+            .map { (1..8).map { "0123456789".random() }.joinToString("") }
+            .joinToString("/") // e.g. 00200000/00000004
+        val schoolClass = "12345".random().toString() + "ABCDEF".random()
+        val birthDate = run {
+            val maxAge = 18 * 12 * 31
+            val minAge = 6 * 12 * 31
+            val upperBound = maxAge - minAge + 1
+            LocalDate.now().minusDays(minAge + Random().nextInt(upperBound).toLong())
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+        }
+
+
+        private val name = object {
+            val nameArray =
+                executeGet("https://www.behindthename.com/api/random.json?usage=ger&gender=${randomGender[0]}&key=lu244794741")
+                    .takeIf { it.isSuccessful }
+                    ?.body()?.string()?.let { jsonString -> Json.parseToJsonElement(jsonString).jsonObject }
+                    ?.get("names")?.jsonArray
+        }
+        val firstName = name.nameArray?.get(1)?.jsonPrimitive?.content ?: "Susanne"
+        val lastName = name.nameArray?.get(0)?.jsonPrimitive?.content ?: "Meier"
+
+        private val address = object {
+            val rawStr = client.newCall(buildRequest()).execute()
                 .takeIf { it.isSuccessful }
-                ?.body()?.string()?.let { jsonString -> Json.parseToJsonElement(jsonString).jsonObject }
-                ?.get("names")?.jsonArray
+                ?.body()?.string()?.let { Json.parseToJsonElement(it).jsonArray }
+                ?.get(0)?.jsonPrimitive?.content
 
-        val firstName = nameArray?.get(1)?.jsonPrimitive?.content ?: "Susanne"
-        val lastName = nameArray?.get(0)?.jsonPrimitive?.content ?: "Meier"
-    }
+            fun buildRequest() = Request.Builder()
+                .url("https://randommer.io/random-address")
+                .post(FormBody.Builder().add("number", "1").add("culture", "de_AT").build())
+                .build()
+        }
 
-    private var address = object {
-
-        private val rawAddressStr = client.newCall(buildRequest()).execute()
-            .takeIf { it.isSuccessful }
-            ?.body()?.string()?.let { Json.parseToJsonElement(it).jsonArray }
-            ?.get(0)?.jsonPrimitive?.content
-
-        private fun buildRequest() = Request.Builder()
-            .url("https://randommer.io/random-address")
-            .post(FormBody.Builder().add("number", "1").add("culture", "de_AT").build())
-            .build()
-
-        val school = rawAddressStr?.split(",")?.toMutableList()?.also { it.removeAt(1) }
+        val school = address.rawStr?.split(",")?.toMutableList()?.also { it.removeAt(1) }
             ?.toList()?.joinToString(",") ?: "Breitenseer Straße 13, 1140, Wien, Austria"
 
-        val county = rawAddressStr?.split(",")?.get(3) ?: "Wien"
+        val county = address.rawStr?.split(",")?.get(3) ?: "Wien"
 
-        val zip = rawAddressStr?.split(",")?.get(2) ?: "1010"
+        val zip = address.rawStr?.split(",")?.get(2) ?: "1010"
+
+
+        var encodedPhoto =
+            executeGetWithApiKey("https://api.generated.photos/api/v1/faces?age=child&page=1&per_page=1&gender=$randomGender")
+                .takeIf { it.isSuccessful }
+                ?.let { jsonResp ->
+                    val body = jsonResp.body()?.string()?.let { Json.parseToJsonElement(it).jsonObject }
+                    val faces = body?.get("faces")?.jsonArray
+                    val face = faces?.get(0)?.jsonObject
+                    val urls = face?.get("urls")?.jsonArray
+                    val picture = urls?.find { url -> url.jsonObject.containsKey(desiredPictureSize) }?.jsonObject
+                    picture?.get(desiredPictureSize)?.jsonPrimitive?.content?.let { loadPicture(it) }
+                } ?: fallbackPhoto
     }
-
-
-    private var encodedPhoto =
-        executeGetWithApiKey("https://api.generated.photos/api/v1/faces?age=child&page=1&per_page=1&gender=$randomGender")
-            .takeIf { it.isSuccessful }
-            ?.let { jsonResp ->
-                val body = jsonResp.body()?.string()?.let { Json.parseToJsonElement(it).jsonObject }
-                val faces = body?.get("faces")?.jsonArray
-                val face = faces?.get(0)?.jsonObject
-                val urls = face?.get("urls")?.jsonArray
-                val picture = urls?.find { url -> url.jsonObject.containsKey(desiredPictureSize) }?.jsonObject
-                picture?.get(desiredPictureSize)?.jsonPrimitive?.content?.let(this::loadPicture)
-            } ?: fallbackPhoto
 
     private fun loadPicture(url: String): String? {
         return executeGet(url).takeIf { it.isSuccessful }?.body()?.bytes()
@@ -76,21 +100,23 @@ class IssuerCredentialRandomDataProvider(
         client.newCall(Request.Builder().url(url).addHeader("Authorization", "API-Key L6VjhxOfJUKWu3xrLnwVIg").build())
             .execute()
 
-    override fun getClaim(subjectId: String, attribute: String) = when (attribute) {
-        "photo" -> Claim(attribute, encodedPhoto, "image/jpeg", defaultLifetime)
-        "schulname" -> Claim(attribute, "Quarto Testschule", "application/text", defaultLifetime)
-        "schuladresse" -> Claim(attribute, address.school, "application/text", defaultLifetime)
-        "schulkennzahl" -> Claim(attribute, "101010", "application/text", defaultLifetime)
-        "schülerkennzahl" -> Claim(attribute, "00200000/00000004", "application/text", defaultLifetime)
-        "vorname" -> Claim(attribute, name.firstName, "application/text", defaultLifetime)
-        "nachname" -> Claim(attribute, name.lastName, "application/text", defaultLifetime)
-        "titelvor" -> Claim(attribute, "", "application/text", defaultLifetime)
-        "titelnach" -> Claim(attribute, "", "application/text", defaultLifetime)
-        "geburtsdatum" -> Claim(attribute, "1997-12-26", "application/text", defaultLifetime)
-        "gültigBis" -> Claim(attribute, "2021-07-31", "application/text", defaultLifetime)
-        "wohnort" -> Claim(attribute, address.county, "application/text", defaultLifetime)
-        "wohnort-plz" -> Claim(attribute, address.zip, "application/text", defaultLifetime)
-        "klasse" -> Claim(attribute, "3B", "application/text", defaultLifetime)
-        else -> null
+    override fun getClaim(subjectId: String, attribute: String) = run { PupilAttributes() }.let {
+        when (attribute) {
+            "photo" -> Claim(attribute, it.encodedPhoto, "image/jpeg", defaultLifetime)
+            "schulname" -> Claim(attribute, it.randomSchool, "application/text", defaultLifetime)
+            "schuladresse" -> Claim(attribute, it.school, "application/text", defaultLifetime)
+            "schulkennzahl" -> Claim(attribute, it.schoolCode, "application/text", defaultLifetime)
+            "schülerkennzahl" -> Claim(attribute, it.pupilIdNumber, "application/text", defaultLifetime)
+            "vorname" -> Claim(attribute, it.firstName, "application/text", defaultLifetime)
+            "nachname" -> Claim(attribute, it.lastName, "application/text", defaultLifetime)
+            "titelvor" -> Claim(attribute, "", "application/text", defaultLifetime)
+            "titelnach" -> Claim(attribute, "", "application/text", defaultLifetime)
+            "geburtsdatum" -> Claim(attribute, it.birthDate, "application/text", defaultLifetime)
+            "gültigBis" -> Claim(attribute, "2021-07-31", "application/text", defaultLifetime)
+            "wohnort" -> Claim(attribute, it.county, "application/text", defaultLifetime)
+            "wohnort-plz" -> Claim(attribute, it.zip, "application/text", defaultLifetime)
+            "klasse" -> Claim(attribute, it.schoolClass, "application/text", defaultLifetime)
+            else -> null
+        }
     }
 }
