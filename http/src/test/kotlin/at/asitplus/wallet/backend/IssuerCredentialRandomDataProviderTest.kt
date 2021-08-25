9e4d304a4f9394257b9aa6c25226d18d45561a65
Lukas Alber <lukas.alber@iaik.tugraz.at>
2021-08-25 19:53:52 +0200

    piture and gender matches, new pics, efficiency correction

--- http/src/test/kotlin/at/asitplus/wallet/backend/IssuerCredentialRandomDataProviderTest.kt
+++ http/src/test/kotlin/at/asitplus/wallet/backend/IssuerCredentialRandomDataProviderTest.kt
@@ -1,6 +1,7 @@
 package at.asitplus.wallet.backend
 
 import at.asitplus.wallet.lib.data.PupilIdAttributes
+import org.jetbrains.annotations.Nullable
 import java.time.Duration
 import java.util.UUID
 import kotlin.test.Test
@@ -13,19 +14,20 @@ class IssuerCredentialRandomDataProviderTest {
 
     @Test
     fun `claims should be different on successive calls`() {
-        val listOfPhotos = (1..10).map { UUID.randomUUID().toString() }
-        val subjectId = UUID.randomUUID().toString()
+        val listOfPhotos = (1..10).map {  "blub" as String? to UUID.randomUUID().toString() }.toMap()
+        val subjectId = UUID.randomUUID().toString() // each subject has own attribute set
+        val subjectId2 = UUID.randomUUID().toString()
         val firstSetOfValues = mutableListOf<String>()
         val secondSetOfValues = mutableListOf<String>()
+        val dataProvider = IssuerCredentialRandomDataProvider(Duration.ofSeconds(1), listOfPhotos)
         for (attribute in PupilIdAttributes.listOfAttributes) {
-            val dataProvider = IssuerCredentialRandomDataProvider(Duration.ofSeconds(1), listOfPhotos)
 
             val firstClaim = dataProvider.getClaim(subjectId, attribute)
             assertNotNull(firstClaim)
             assertEquals(attribute, firstClaim.name)
             assertTrue(firstClaim.value.isNotEmpty())
 
-            val secondClaim = dataProvider.getClaim(subjectId, attribute)
+            val secondClaim = dataProvider.getClaim(subjectId2, attribute)
             assertNotNull(secondClaim)
             assertEquals(attribute, secondClaim.name)
             assertTrue(secondClaim.value.isNotEmpty())
@@ -33,8 +35,7 @@ class IssuerCredentialRandomDataProviderTest {
             firstSetOfValues += firstClaim.value
             secondSetOfValues += secondClaim.value
         }
-        assertNotEquals(firstSetOfValues, secondSetOfValues) //
-
+        assertNotEquals(firstSetOfValues, secondSetOfValues)
     }
 
 }
