c828116a91ddb56ac7901f11064a4561e7bfa1ef
Christian Kollmann <christian.kollmann@a-sit.at>
2021-09-10 13:22:17 +0200

    Bump version to 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.3" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-    kotlin("jvm") version "1.5.21" apply false
-    kotlin("plugin.spring") version "1.5.21" apply false
-    kotlin("plugin.jpa") version "1.5.21" apply false
-    kotlin("plugin.serialization") version "1.5.21" apply false
+    kotlin("jvm") version "1.5.30" apply false
+    kotlin("plugin.spring") version "1.5.30" apply false
+    kotlin("plugin.jpa") version "1.5.30" apply false
+    kotlin("plugin.serialization") version "1.5.30" apply false
 }
 allprojects {
     group = "at.asitplus.wallet"
-    version = "0.0.1-SNAPSHOT"
+    version = "0.1.0"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
@@ -20,7 +20,7 @@ allprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = "1.8"
         }
     }
 
--- http/build.gradle.kts
+++ http/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "at.asitplus.wallet"
-version = "0.0.1-SNAPSHOT"
+version = "0.1.0"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
@@ -50,7 +50,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
 
