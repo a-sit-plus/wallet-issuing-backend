d6e9f9d613149212474928178fe3371a9117a9ec
Christian Kollmann <christian.kollmann@a-sit.at>
2022-03-18 11:00:39 +0100

    Add documentation for configuration properties

--- http/src/main/kotlin/at/asitplus/wallet/backend/BackendConfigurationProperties.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/BackendConfigurationProperties.kt
@@ -16,21 +16,50 @@ data class BackendConfigurationProperties(
      * Lifetime of the credentials issued, e.g. 60 minutes or 6 months
      */
     val credentialLifetime: Duration = Duration.ofMinutes(60),
+    /**
+     * Key used for signing issued credentials
+     * TODO Connection to HSM Facade and AERA
+     */
     val issuerKey: KeyConfiguration = KeyConfiguration(),
+    /**
+     * Configure debug endpoints
+     */
     val debug: DebugConfigurationProperties = DebugConfigurationProperties(),
+    /**
+     * Configure authentication of external services at this service
+     */
     val authn: AuthnConfigurationProperties = AuthnConfigurationProperties(),
 )
 
 @ConstructorBinding
 data class DebugConfigurationProperties(
+    /**
+     * Whether to enable debug endpoints at all
+     */
     val enabled: Boolean = false,
+    /**
+     * Size of QR Codes, displayed only on debug endpoints
+     */
     val qrCodeSize: Int = 400,
+    /**
+     * Whether to use random data for issued credentials
+     */
+    val randomData: Boolean = false,
+    /**
+     * Location of random photos to be used for issued credentials
+     */
     val randomPhotoLocation: URI = URI.create("file:photos/"),
 )
 
 @ConstructorBinding
 data class AuthnConfigurationProperties(
+    /**
+     * Lifetime of challenges sent to clients, e.g. during device binding
+     */
     val challengeTimeoutSeconds: Int = 60,
+    /**
+     * Valid API keys for revocation endpoints
+     */
     val apiKeys: Collection<ApiKeyConfigurationProperties> = listOf(),
 )
 
