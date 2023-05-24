f0cb58a2f765b3a494e2d582b6768fd189c1c1e9
Christian Kollmann <christian.kollmann@a-sit.at>
2023-05-24 08:19:57 +0200

    Extract methods for OpenId4VCI into separate controller

--- http/src/main/kotlin/at/asitplus/wallet/backend/controller/EidasIdController.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/controller/EidasIdController.kt
@@ -9,16 +9,6 @@ import at.asitplus.wallet.backend.data.CredentialDataProvider
 import at.asitplus.wallet.backend.data.EidasCredentialDataProvider
 import at.asitplus.wallet.backend.service.IssueCredentialAdapter
 import at.asitplus.wallet.lib.aries.NextMessage
-import at.asitplus.wallet.lib.oidvci.AuthorizationRequestParameters
-import at.asitplus.wallet.lib.oidvci.CredentialRequestParameters
-import at.asitplus.wallet.lib.oidvci.IssuerMetadata
-import at.asitplus.wallet.lib.oidvci.IssuerService
-import at.asitplus.wallet.lib.oidvci.OAuth2Error
-import at.asitplus.wallet.lib.oidvci.OAuth2Exception
-import at.asitplus.wallet.lib.oidvci.TokenRequestParameters
-import at.asitplus.wallet.lib.oidvci.decodeFromPostBody
-import at.asitplus.wallet.lib.oidvci.decodeFromUrlQuery
-import at.asitplus.wallet.lib.oidvci.jsonSerializer
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.qrcode.QRCodeWriter
@@ -29,13 +19,8 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.Clock
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -46,10 +31,6 @@ import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.util.UriComponentsBuilder
@@ -63,7 +44,6 @@ import javax.servlet.http.HttpServletRequest
  * Provides endpoints in the EIDAS deployment:
  * - REST for Wallet App to get credentials (with a device binding)
  * - MVC for Browser to display QR Code to initialize Wallet App
- * - OID4VCI for issuing credentials without a device binding
  */
 @Profile(ProfileConstants.EIDASID)
 @RestController
@@ -72,7 +52,6 @@ class EidasIdController(
     private val extNonceAuthnService: ExtNonceAuthnService,
     private val configurationProperties: BackendConfigurationProperties,
     private val credentialDataProvider: CredentialDataProvider,
-    private val issuerService: IssuerService,
 ) {
 
     @Operation(
@@ -114,102 +93,29 @@ class EidasIdController(
         return when (val result = issueCredentialAdapter.parseMessage(body)) {
             is NextMessage.Result<*> -> ResponseEntity.ok().build<String>()
                 .also { Napier.i("/eidasid/issue returns HTTP 200: Finished") }
+
             is NextMessage.Send -> ResponseEntity.ok(result.message)
                 .also {
                     // TODO: No idea what data is sent around here... For now I treat it as critical
                     Napier.i("/eidasid/issue returns HTTP 200")
                     Napier.v("message: ${result.message.take(128)}...")
                 }
+
             is NextMessage.Error -> ResponseEntity.status(HttpStatus.BAD_REQUEST).build<String>()
                 .also { Napier.w("/eidasid/issue returns HTTP 400: Incorrect protocol state") }
+
             is NextMessage.SendProblemReport -> ResponseEntity.ok(result.message)
                 .also {
                     Napier.i("/eidasid/issue returns HTTP 200: Problem Report")
                     Napier.v("Problem report: ${result.message}")
                 }
+
             is NextMessage.ReceivedProblemReport -> ResponseEntity.ok().build<String>()
                 .also {
                     Napier.i("/eidasid/issue returns HTTP 200: Received Problem Report")
                     Napier.v("Received Problem Report ${result.message}")
                 }
         }.also { request.logout() }
-    }
-
-    @GetMapping("/.well-known/openid-credential-issuer")
-    fun metadata(): ResponseEntity<IssuerMetadata> {
-        val metadata = issuerService.metadata()
-        Napier.i("/.well-known/openid-credential-issuer returns $metadata")
-        return ResponseEntity.ok(metadata)
-    }
-
-    @RequestMapping("/authorize", method = [RequestMethod.POST, RequestMethod.GET])
-    fun authorize(
-        @RequestParam requestParams: Map<String, String>,
-        @RequestBody requestBody: String?
-    ): ResponseEntity<String> {
-        Napier.i("/authorize called")
-        Napier.v("/authorize called with $requestParams and $requestBody")
-        val params: AuthorizationRequestParameters =
-            if (requestBody.isNullOrEmpty()) requestParams.decodeFromUrlQuery()
-            else requestBody.decodeFromPostBody()
-        val location = issuerService.authorize(params)
-        Napier.d("/authorize returns $location")
-        return buildOidcRedirect(location)
-    }
-
-    @RequestMapping("/token", method = [RequestMethod.POST])
-    fun token(@RequestBody requestBody: String): ResponseEntity<*> {
-        Napier.i("/token called")
-        Napier.v("/token called with $requestBody")
-        val params: TokenRequestParameters = requestBody.decodeFromPostBody()
-            ?: return buildOidcErrorResponse("invalid_request")
-        return try {
-            val result = issuerService.token(params)
-            Napier.d("/token returns $result")
-            ResponseEntity.ok(Json.encodeToString(result))
-        } catch (e: OAuth2Exception) {
-            Napier.w("/token error $e, $e.error")
-            buildOidcErrorResponse(e.error)
-        }
-    }
-
-    @RequestMapping("/credential", method = [RequestMethod.POST])
-    fun credential(
-        @RequestBody requestBody: String,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) authorizationHeader: String
-    ): ResponseEntity<*> = runBlocking {
-        Napier.i("/credential called")
-        Napier.v("/credential called with $authorizationHeader and $requestBody")
-        return@runBlocking suspendingCredential(authorizationHeader, requestBody)
-    }
-
-    private suspend fun suspendingCredential(
-        authorizationHeader: String,
-        requestBody: String
-    ): ResponseEntity<out Any> {
-        val params: CredentialRequestParameters = jsonSerializer.decodeFromString(requestBody)
-            ?: return buildOidcErrorResponse("invalid_request")
-        return try {
-            val credential = issuerService.credential(authorizationHeader, params)
-            Napier.d("/credential returns $credential")
-            ResponseEntity.ok(jsonSerializer.encodeToString(credential))
-        } catch (e: OAuth2Exception) {
-            Napier.w("/credential error $e, $e.error")
-            buildOidcErrorResponse(e.error)
-        }
-    }
-
-    private fun buildOidcRedirect(location: String): ResponseEntity<String> {
-        return ResponseEntity
-            .status(HttpStatus.FOUND)
-            .header(HttpHeaders.LOCATION, location)
-            .build()
-    }
-
-    private fun buildOidcErrorResponse(error: String): ResponseEntity<OAuth2Error> {
-        return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(OAuth2Error(error = error))
     }
 
     /**
---
+++ http/src/main/kotlin/at/asitplus/wallet/backend/controller/OpenId4VciController.kt
@@ -0,0 +1,119 @@
+package at.asitplus.wallet.backend.controller
+
+import at.asitplus.wallet.backend.ProfileConstants
+import at.asitplus.wallet.lib.oidvci.AuthorizationRequestParameters
+import at.asitplus.wallet.lib.oidvci.CredentialRequestParameters
+import at.asitplus.wallet.lib.oidvci.IssuerMetadata
+import at.asitplus.wallet.lib.oidvci.IssuerService
+import at.asitplus.wallet.lib.oidvci.OAuth2Error
+import at.asitplus.wallet.lib.oidvci.OAuth2Exception
+import at.asitplus.wallet.lib.oidvci.TokenRequestParameters
+import at.asitplus.wallet.lib.oidvci.decodeFromPostBody
+import at.asitplus.wallet.lib.oidvci.decodeFromUrlQuery
+import at.asitplus.wallet.lib.oidvci.jsonSerializer
+import io.github.aakira.napier.Napier
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+/**
+ * Provides endpoints in the EIDAS deployment:
+ * - OID4VCI for issuing credentials after a device binding
+ */
+@Profile(ProfileConstants.EIDASID)
+@RestController
+class OpenId4VciController(
+    private val issuerService: IssuerService,
+) {
+
+    @GetMapping("/.well-known/openid-credential-issuer")
+    fun metadata(): ResponseEntity<IssuerMetadata> {
+        val metadata = issuerService.metadata()
+        Napier.i("/.well-known/openid-credential-issuer returns $metadata")
+        return ResponseEntity.ok(metadata)
+    }
+
+    @RequestMapping("/authorize", method = [RequestMethod.POST, RequestMethod.GET])
+    fun authorize(
+        @RequestParam requestParams: Map<String, String>,
+        @RequestBody requestBody: String?
+    ): ResponseEntity<String> {
+        Napier.i("/authorize called")
+        Napier.v("/authorize called with $requestParams and $requestBody")
+        val params: AuthorizationRequestParameters =
+            if (requestBody.isNullOrEmpty()) requestParams.decodeFromUrlQuery()
+            else requestBody.decodeFromPostBody()
+        val location = issuerService.authorize(params)
+        Napier.d("/authorize returns $location")
+        return buildOidcRedirect(location)
+    }
+
+    @RequestMapping("/token", method = [RequestMethod.POST])
+    fun token(@RequestBody requestBody: String): ResponseEntity<*> {
+        Napier.i("/token called")
+        Napier.v("/token called with $requestBody")
+        val params: TokenRequestParameters = requestBody.decodeFromPostBody()
+            ?: return buildOidcErrorResponse("invalid_request")
+        return try {
+            val result = issuerService.token(params)
+            Napier.d("/token returns $result")
+            ResponseEntity.ok(Json.encodeToString(result))
+        } catch (e: OAuth2Exception) {
+            Napier.w("/token error $e, $e.error")
+            buildOidcErrorResponse(e.error)
+        }
+    }
+
+    @RequestMapping("/credential", method = [RequestMethod.POST])
+    fun credential(
+        @RequestBody requestBody: String,
+        @RequestHeader(HttpHeaders.AUTHORIZATION) authorizationHeader: String
+    ): ResponseEntity<*> = runBlocking {
+        Napier.i("/credential called")
+        Napier.v("/credential called with $authorizationHeader and $requestBody")
+        return@runBlocking suspendingCredential(authorizationHeader, requestBody)
+    }
+
+    private suspend fun suspendingCredential(
+        authorizationHeader: String,
+        requestBody: String
+    ): ResponseEntity<out Any> {
+        val params: CredentialRequestParameters = jsonSerializer.decodeFromString(requestBody)
+            ?: return buildOidcErrorResponse("invalid_request")
+        return try {
+            val credential = issuerService.credential(authorizationHeader, params)
+            Napier.d("/credential returns $credential")
+            ResponseEntity.ok(jsonSerializer.encodeToString(credential))
+        } catch (e: OAuth2Exception) {
+            Napier.w("/credential error $e, $e.error")
+            buildOidcErrorResponse(e.error)
+        }
+    }
+
+    private fun buildOidcRedirect(location: String): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.FOUND)
+            .header(HttpHeaders.LOCATION, location)
+            .build()
+    }
+
+    private fun buildOidcErrorResponse(error: String): ResponseEntity<OAuth2Error> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(OAuth2Error(error = error))
+    }
+
+}
