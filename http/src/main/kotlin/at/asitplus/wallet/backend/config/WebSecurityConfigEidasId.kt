8070998700dd866944293510a697b1cf285f29a3
Christian Kollmann <christian.kollmann@a-sit.at>
2023-11-30 13:01:26 +0100

    Fix OIDC authn from app

--- http/src/main/kotlin/at/asitplus/wallet/backend/config/WebSecurityConfigEidasId.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/config/WebSecurityConfigEidasId.kt
@@ -1,16 +1,16 @@
 package at.asitplus.wallet.backend.config
 
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.session.MapSessionRepository
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession
+import org.springframework.session.web.http.CookieHttpSessionIdResolver
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver
 import org.springframework.session.web.http.HttpSessionIdResolver
 import java.util.concurrent.ConcurrentHashMap
@@ -27,13 +27,8 @@ class WebSecurityConfigEidasId {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf { it.disable() }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) }
-            .exceptionHandling {
-                it.defaultAuthenticationEntryPointFor(
-                    LoginUrlAuthenticationEntryPoint("/login"),
-                    AntPathRequestMatcher("/**")
-                )
-            }.logout {
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.ALWAYS) }
+            .logout {
                 it.invalidateHttpSession(true)
                     .clearAuthentication(true)
                     .logoutSuccessUrl("/")
@@ -55,8 +50,31 @@ class WebSecurityConfigEidasId {
      */
     @Bean
     fun httpSessionIdResolver(): HttpSessionIdResolver {
-        return HeaderHttpSessionIdResolver.xAuthToken()
+        return DelegatingSessionIdResolver(CookieHttpSessionIdResolver(), HeaderHttpSessionIdResolver.xAuthToken())
     }
 
 }
 
+/**
+ * Used in EIDAS deployments to set session identifier to
+ * header `X-Auth-Token` and cookie `SESSION`.
+ */
+class DelegatingSessionIdResolver(private vararg val resolvers: HttpSessionIdResolver) : HttpSessionIdResolver {
+
+    override fun resolveSessionIds(request: HttpServletRequest?): MutableList<String> {
+        return resolvers.map { it.resolveSessionIds(request) }.flatten().toMutableList()
+    }
+
+    override fun setSessionId(request: HttpServletRequest?, response: HttpServletResponse?, sessionId: String?) {
+        resolvers.forEach {
+            it.setSessionId(request, response, sessionId)
+        }
+    }
+
+    override fun expireSession(request: HttpServletRequest?, response: HttpServletResponse?) {
+        resolvers.forEach {
+            it.expireSession(request, response)
+        }
+    }
+
+}
