435d07cdee72ea62fd2238e53aa8a06f37fede32
Christian Kollmann <christian.kollmann@a-sit.at>
2024-06-05 10:20:21 +0200

    Update to vclib 3.7.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object VersionsBackend {
     const val ida = "3.4.0"
-    const val eupid = "1.0.0"
+    const val eupid = "2.0.1"
     const val zxing = "3.5.2"
     const val `google-cbor` = "0.01.02"
     const val hsmf = "0.8.3"
@@ -8,12 +8,12 @@ object VersionsBackend {
     const val mockito = "5.1.0"
     const val okhttp = "4.11.0"
     const val scrimage = "4.0.39"
-    const val vclib = "3.6.0"
+    const val vclib = "3.7.0"
     const val attestation = "0.5.2"
     const val nimbus = "9.37.1"
 
     object spring {
-        const val boot = "3.1.3"
+        const val boot = "3.1.9"
         const val `dependency-management` = "1.1.3"
         const val `cloud-config-client` = "4.0.4"
         const val `admin-starter-client` = "3.1.6"
