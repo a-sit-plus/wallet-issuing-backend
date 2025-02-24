6b82724f213ae436040f631fe2467c592eb1cea2
Christian Kollmann <christian.kollmann@a-sit.at>
2025-02-24 17:47:09 +0100

    Fix status list token encoding

--- http/src/main/kotlin/at/asitplus/wallet/backend/service/RevocationListWriter.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/service/RevocationListWriter.kt
@@ -2,10 +2,13 @@ package at.asitplus.wallet.backend.service
 
 import at.asitplus.wallet.backend.config.BackendConfigurationProperties
 import at.asitplus.wallet.lib.agent.Issuer
-import at.asitplus.wallet.lib.data.vckJsonSerializer
+import at.asitplus.wallet.lib.data.StatusListToken
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.agents.communication.primitives.StatusListTokenMediaType
+import at.asitplus.wallet.lib.iso.vckCborSerializer
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.encodeToString
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToByteArray
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import kotlin.io.path.*
@@ -24,19 +27,36 @@ class RevocationListWriter(
      * Since move is an atomic operation (at least on Linux), the read
      * operations should never read a partial file.
      */
+    @OptIn(ExperimentalSerializationApi::class)
     fun writeRevocationList(timePeriod: Int) {
         Dispatchers.IO.run {
             runBlocking {
-                log.info("Writing revocation list for $timePeriod")
-                Path(configurationProperties.revocationList.path).createDirectories()
-                val destinationFile = Path(configurationProperties.revocationList.path, timePeriod.toString())
-                issuer.buildStatusList(timePeriod)?.let { list ->
-                    val text = vckJsonSerializer.encodeToString(list)
-                    createTempFile().apply {
-                        writeText(text)
-                        moveTo(destinationFile, true)
+                with(configurationProperties.revocationList) {
+                    log.info("Writing revocation list for $timePeriod")
+                    Path(jwtPath).createDirectories()
+                    Path(jwtPath, timePeriod.toString()).let { destinationFile ->
+                        issuer.provideStatusListToken(listOf(StatusListTokenMediaType.Jwt)).let { token ->
+                            val content = token.second as StatusListToken.StatusListJwt
+                            val text = content.value.serialize()
+                            createTempFile().apply {
+                                writeText(text)
+                                moveTo(destinationFile, true)
+                            }
+                            log.info("Wrote JWT status token for $timePeriod to ${destinationFile.pathString} with ${text.length} chars")
+                        }
                     }
-                    log.info("Wrote revocation list for $timePeriod to ${destinationFile.pathString} with ${text.length} chars")
+                    Path(cwtPath).createDirectories()
+                    Path(cwtPath, timePeriod.toString()).let { destinationFile ->
+                        issuer.provideStatusListToken(listOf(StatusListTokenMediaType.Cwt)).let { token ->
+                            val content = token.second as StatusListToken.StatusListCwt
+                            val bytes = vckCborSerializer.encodeToByteArray(content.value)
+                            createTempFile().apply {
+                                writeBytes(bytes)
+                                moveTo(destinationFile, true)
+                            }
+                            log.info("Wrote CWT status token for $timePeriod to ${destinationFile.pathString} with ${bytes.size} bytes")
+                        }
+                    }
                 }
             }
         }
--- http/src/test/kotlin/at/asitplus/wallet/backend/spring/PublicControllerTest.kt
+++ http/src/test/kotlin/at/asitplus/wallet/backend/spring/PublicControllerTest.kt
@@ -1,6 +1,7 @@
 package at.asitplus.wallet.backend.spring
 
 import at.asitplus.wallet.lib.agent.TimePeriodProvider
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.MediaTypes
 import kotlinx.datetime.Clock
 import org.hamcrest.Matchers.emptyString
 import org.hamcrest.Matchers.not
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -23,55 +25,63 @@ class PublicControllerTest {
     private lateinit var timePeriodProvider: TimePeriodProvider
 
     @Test
-    fun `GET VC status list with If-None-Match in second request`() {
+    fun `GET CWT status list with If-None-Match in second request`() {
         Thread.sleep(1000L) // wait for RevocationListScheduler to write the revocation list
         val timePeriod = timePeriodProvider.getRelevantTimePeriods(Clock.System).first()
         val firstResult = mockMvc.get("/credentials/status/$timePeriod") {
+            accept(MediaType.parseMediaType(MediaTypes.Application.STATUSLIST_CWT))
         }.andExpect {
             status { isOk() }
             content { string(not(emptyString())) }
+            header { stringValues(HttpHeaders.CONTENT_TYPE, MediaTypes.Application.STATUSLIST_CWT) }
             header { exists(HttpHeaders.ETAG) }
             header { exists(HttpHeaders.CACHE_CONTROL) }
         }.andReturn()
 
         mockMvc.get("/credentials/status/$timePeriod") {
+            accept(MediaType.parseMediaType(MediaTypes.Application.STATUSLIST_CWT))
             headers {
                 ifNoneMatch = listOf(firstResult.response.getHeader(HttpHeaders.ETAG))
             }
         }.andExpect {
             status { isNotModified() }
             content { string(emptyString()) }
+            header { stringValues(HttpHeaders.CONTENT_TYPE, MediaTypes.Application.STATUSLIST_CWT) }
             header { exists(HttpHeaders.ETAG) }
             header { exists(HttpHeaders.CACHE_CONTROL) }
         }.andReturn()
     }
 
     @Test
-    fun `GET VC status list, with If-Modified-Since in second request`() {
+    fun `GET JWT status list token, with If-Modified-Since in second request`() {
         Thread.sleep(1000L) // wait for RevocationListScheduler to write the revocation list
         val timePeriod = timePeriodProvider.getRelevantTimePeriods(Clock.System).first()
         val firstResult = mockMvc.get("/credentials/status/$timePeriod") {
+            accept(MediaType.parseMediaType(MediaTypes.Application.STATUSLIST_JWT))
         }.andExpect {
             status { isOk() }
             content { string(not(emptyString())) }
+            header { stringValues(HttpHeaders.CONTENT_TYPE, MediaTypes.Application.STATUSLIST_JWT) }
             header { exists(HttpHeaders.LAST_MODIFIED) }
             header { exists(HttpHeaders.CACHE_CONTROL) }
         }.andReturn()
 
         mockMvc.get("/credentials/status/$timePeriod") {
+            accept(MediaType.parseMediaType(MediaTypes.Application.STATUSLIST_JWT))
             headers {
                 ifModifiedSince = firstResult.response.getDateHeader(HttpHeaders.LAST_MODIFIED)
             }
         }.andExpect {
             status { isNotModified() }
             content { string(emptyString()) }
+            header { stringValues(HttpHeaders.CONTENT_TYPE, MediaTypes.Application.STATUSLIST_JWT) }
             header { exists(HttpHeaders.LAST_MODIFIED) }
             header { exists(HttpHeaders.CACHE_CONTROL) }
         }.andReturn()
     }
 
     @Test
-    fun `GET VC status list with invalid period`() {
+    fun `GET status list with invalid period`() {
         mockMvc.get("/credentials/status/${timePeriodProvider.getRelevantTimePeriods(Clock.System).max() * 2}") {
         }.andExpect {
             status { isNotFound() }
