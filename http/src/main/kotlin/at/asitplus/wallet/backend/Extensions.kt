28e12966c4ccd1cf23151b506802486aa15c8ac4
Christian Kollmann <christian.kollmann@a-sit.at>
2025-08-22 13:26:53 +0200

    Remove unused code

--- http/src/main/kotlin/at/asitplus/wallet/backend/Extensions.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/Extensions.kt
@@ -1,13 +1,14 @@
 package at.asitplus.wallet.backend
 
 import org.springframework.web.util.UriComponentsBuilder
+import java.security.MessageDigest
 
 object Extensions {
 
-    fun appendPath(url: String, vararg path: String) =
-        UriComponentsBuilder.fromHttpUrl(url).pathSegment(*path).toUriString()
+    fun appendPath(url: String, vararg path: String): String =
+        UriComponentsBuilder.fromUriString(url).pathSegment(*path).toUriString()
 
-    fun ByteArray.sha256(): ByteArray = java.security.MessageDigest.getInstance("SHA-256").digest(this)
+    fun ByteArray.sha256(): ByteArray = MessageDigest.getInstance("SHA-256").digest(this)
 
 }
 
