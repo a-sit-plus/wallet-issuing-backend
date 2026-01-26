9afc999bc30ee83fa6183595b657d6332417a7ce
Christian Kollmann <christian.kollmann@a-sit.at>
2026-01-26 13:10:18 +0100

    Split up data extraction

--- http/src/main/kotlin/at/asitplus/wallet/backend/config/DataExtractor.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/config/DataExtractor.kt
@@ -13,7 +13,6 @@ import at.asitplus.wallet.cor.CertificateOfResidenceScheme
 import at.asitplus.wallet.ehic.EhicScheme
 import at.asitplus.wallet.eupid.EuPidCredential
 import at.asitplus.wallet.eupid.EuPidScheme
-import at.asitplus.wallet.eupid.IsoIec5218Gender
 import at.asitplus.wallet.eupid.PlaceOfBirth
 import at.asitplus.wallet.eupidsdjwt.EuPidSdJwtScheme
 import at.asitplus.wallet.lib.agent.ClaimToBeIssued
@@ -22,33 +21,15 @@ import at.asitplus.wallet.lib.agent.CredentialToBeIssued
 import at.asitplus.wallet.lib.data.ConstantIndex
 import at.asitplus.wallet.lib.data.LocalDateOrInstant
 import at.asitplus.wallet.lib.jws.JwsHeaderModifierFun
-import at.asitplus.wallet.mdl.DrivingPrivilege
-import at.asitplus.wallet.mdl.IsoSexEnum
 import at.asitplus.wallet.mdl.MobileDrivingLicenceDataElements
 import at.asitplus.wallet.mdl.MobileDrivingLicenceScheme
 import at.asitplus.wallet.por.PowerOfRepresentationDataElements
 import at.asitplus.wallet.por.PowerOfRepresentationScheme
 import at.asitplus.wallet.taxid.TaxIdScheme
 import io.github.aakira.napier.Napier
-import io.matthewnelson.encoding.base64.Base64
-import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
 import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
-import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.format
-import kotlinx.datetime.minus
-import kotlinx.datetime.toLocalDateTime
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonPrimitive
-import java.nio.charset.Charset
-import java.util.*
 import kotlin.random.Random
-import kotlin.random.nextUInt
-import kotlin.time.Clock
 import kotlin.time.Instant
 
 fun ConstantIndex.CredentialScheme.buildSdJwtClaims(
@@ -245,36 +226,6 @@ private fun OidcAddressClaim.parseOidcAddress(): Address? =
         )
     } else null
 
-private fun OidcUserInfoExtended.parseIdAustriaAddress(): Address? =
-    getClaimAsString("urn:eidgvat:attributes.mainAddress")?.let { idaAddress ->
-        runCatching {
-            idaAddress.parseIdAustriaAddress()
-        }.getOrNull()
-    }
-
-private fun String.parseIdAustriaAddress(): Address? {
-    val json = Json.parseToJsonElement(
-        decodeToByteArray(Base64()).toString(Charset.defaultCharset())
-    ) as? JsonObject
-    val postCode = json.getPrimitiveContent("Postleitzahl")
-    val city = json.getPrimitiveContent("Ortschaft")
-    val street = json.getPrimitiveContent("Strasse")
-    val locator = json.getPrimitiveContent("Hausnummer")
-    return if (postCode != null && city != null && street != null && locator != null) {
-        Address(
-            postCode = postCode,
-            city = city,
-            state = postCode.toState(),
-            street = street,
-            locator = locator.toIntOrNull() ?: randomAddressLocator
-        )
-    } else {
-        null
-    }
-}
-
-private fun JsonObject?.getPrimitiveContent(key: String) = (this?.get(key) as? JsonPrimitive)?.content
-
 fun OidcUserInfoExtended.buildPorClaims(iss: Instant, exp: Instant, useSd: Boolean) =
     with(PowerOfRepresentationDataElements) {
         listOfNotNull(
@@ -431,7 +382,6 @@ fun OidcUserInfoExtended.buildCorClaims(iss: Instant, exp: Instant, useSd: Boole
         )
     }
 
-
 fun OidcUserInfoExtended.buildMdlClaims(useSd: Boolean) =
     with(MobileDrivingLicenceDataElements) {
         val (postCode, city, state, street, locator) = addressOrRandom
@@ -451,8 +401,8 @@ fun OidcUserInfoExtended.buildMdlClaims(useSd: Boolean) =
             claim(UN_DISTINGUISHING_SIGN, useSd) { unDistinguishingSign },
             claim(ADMINISTRATIVE_NUMBER, useSd) { randomIdentifier },
             claim(SEX, useSd) { sex },
-            claim(HEIGHT, useSd) { Random.nextUInt(150u, 210u) },
-            claim(WEIGHT, useSd) { Random.nextUInt(60u, 120u) },
+            claim(HEIGHT, useSd) { randomHeight },
+            claim(WEIGHT, useSd) { randomWeight },
             claim(EYE_COLOUR, useSd) { randomEyeColour },
             claim(HAIR_COLOUR, useSd) { randomHairColour },
             claim(BIRTH_PLACE, useSd) { randomAddress.city },
@@ -503,289 +453,6 @@ fun OidcUserInfoExtended.buildAgeClaims(useSd: Boolean) =
             claim(AGE_OVER_68, useSd) { ageOver68 },
         )
     }
-
-val fakeDrivingPrivilege
-    get() = DrivingPrivilege(
-        vehicleCategoryCode = "B",
-        issueDate = issueDate,
-        expiryDate = expiryDate,
-    )
-
-val OidcUserInfoExtended.dateOfBirth: LocalDate
-    get() = userInfo.birthDate?.let { runCatching { LocalDate.parse(it) }.getOrNull() }
-        ?: randomDateOfBirth
-
-val OidcUserInfoExtended.email
-    get() = userInfo.email
-        ?: "info@example.com"
-
-val OidcUserInfoExtended.phoneNumber
-    get() = userInfo.phoneNumber?.replace("-", "")
-        ?: "+498999998001"
-
-val OidcUserInfoExtended.sex
-    get() = getClaimAsString("urn:eidgvat:attributes.gender")?.toIsoSexEnum()
-        ?: IsoSexEnum.NOT_KNOWN
-
-val OidcUserInfoExtended.gender
-    get() = getClaimAsString("urn:eidgvat:attributes.gender")?.toIsoGenderEnum()
-        ?: IsoIec5218Gender.NOT_KNOWN
-
-fun String.toIsoSexEnum() = when (this) {
-    "W" -> IsoSexEnum.FEMALE
-    "M" -> IsoSexEnum.MALE
-    else -> IsoSexEnum.NOT_KNOWN
-}
-
-fun String.toIsoGenderEnum() = when (this) {
-    "W" -> IsoIec5218Gender.FEMALE
-    "M" -> IsoIec5218Gender.MALE
-    else -> IsoIec5218Gender.NOT_KNOWN
-}
-
-val OidcUserInfoExtended.ageOver12
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_12")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(12)))
-
-val OidcUserInfoExtended.ageOver13
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_13")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(13)))
-
-val OidcUserInfoExtended.ageOver14
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_14")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(14)))
-
-val OidcUserInfoExtended.ageOver16
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_16")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(16)))
-
-val OidcUserInfoExtended.ageOver18: Boolean
-    get() = userInfo.ageOver18
-        ?: getClaimAsString("org.iso.18013.5.1:age_over_18")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(18)))
-
-val OidcUserInfoExtended.ageOver21: Boolean
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_21")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(21)))
-
-val OidcUserInfoExtended.ageOver25: Boolean
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_25")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(25)))
-
-val OidcUserInfoExtended.ageOver60: Boolean
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_60")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(60)))
-
-val OidcUserInfoExtended.ageOver62: Boolean
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_62")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(62)))
-
-val OidcUserInfoExtended.ageOver65: Boolean
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_65")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(65)))
-
-val OidcUserInfoExtended.ageOver68: Boolean
-    get() = getClaimAsString("org.iso.18013.5.1:age_over_68")?.toBoolean()
-        ?: (dateOfBirth < Clock.System.now().toLocalDate().minus(DatePeriod(68)))
-
-fun Instant.toLocalDate() = toLocalDateTime(TimeZone.currentSystemDefault()).date
-
-val OidcUserInfoExtended.ageInYears: UInt
-    get() = (Clock.System.now().toLocalDate().minus(dateOfBirth)).years.toUInt()
-
-val OidcUserInfoExtended.portrait: ByteArray?
-    get() = userInfo.picture?.decodeToByteArray(Base64())
-        ?: getClaimAsString("org.iso.18013.5.1:portrait")?.decodeToByteArray(Base64())
-
-val OidcUserInfoExtended.portraitCaptureDate: LocalDate
-    get() = getClaimAsString("org.iso.18013.5.1:portrait_capture_date")
-        ?.let { runCatching { LocalDate.parse(it) }.getOrNull() }
-        ?: LocalDate(2020, Random.nextInt(1, 12), Random.nextInt(1, 28))
-
-val OidcUserInfoExtended.arrivalDate: LocalDate
-    get() = getClaimAsString("urn:eidgvat:attributes.mainAddressRegistrationDate")
-        ?.let { runCatching { LocalDate.parse(it) }.getOrNull() }
-        ?: LocalDate(2000, Random.nextInt(1, 12), Random.nextInt(1, 28))
-
-val OidcUserInfoExtended.nationality: String
-    get() = getClaimAsString("urn:eidgvat:attributes.nationality")?.let {
-        runCatching {
-            Json.parseToJsonElement(it).jsonArray.first().jsonPrimitive.content
-        }.getOrNull()
-            ?.mapToAlpha2()
-    } ?: "AT"
-
-fun String.mapToAlpha2() = when (this) {
-    "AUT" -> "AT"
-    "DEU" -> "DE"
-    "CHE" -> "CH"
-    else -> "XX"
-}
-
-val OidcUserInfoExtended.legalName: String
-    get() = getClaimAsString("urn:pvpgvat:oidc.mandator_legal_person_full_name")
-        ?: "A-SIT Plus GmbH"
-
-val OidcUserInfoExtended.legalPersonIdentifier: String
-    get() = getClaimAsString("urn:pvpgvat:oidc.mandator_legal_person_source_pin")
-        ?: "XFN+436920f"
-
-fun OidcUserInfoExtended.getClaimAsString(key: String): String? {
-    val element = jsonObject[key]
-    if (element is JsonPrimitive) {
-        return element.content
-    }
-    return element?.toString()
-}
-
-val OidcUserInfoExtended.socialSecurityNumber: String
-    get() = "1111" + dateOfBirth.format(LocalDate.Format {
-        day()
-        monthNumber()
-        year()
-    })
-
-private fun formatAddress(street: String, locator: Int, postalCode: String, city: String) =
-    "$street $locator, $postalCode $city"
-
-private fun claim(key: String, useSd: Boolean, value: () -> Any?): ClaimToBeIssued? =
-    value()?.let { ClaimToBeIssued(key, it, useSd) }
-
-private val randomIdentifier: String
-    get() = UUID.randomUUID().toString()
-private val expiryDate = LocalDate.parse("2026-12-31")
-private val issueDate = LocalDate.parse("2023-01-01")
-private const val trustAnchor = "https://wallet.a-sit.at/"
-private const val issuingCountry = "AT"
-private const val issuingJurisdiction = "AT-0"
-private const val issuingAuthority = "Miniwahr"
-private const val authenticSource = "Ministry of Love"
-private const val unDistinguishingSign = "A"
-private const val fallbackBirthCountry = "AT"
-private const val fallbackAddressCountry = "AT"
-
-private fun String.toState(): String = when {
-    this.startsWith("1") -> "Wien"
-    this.startsWith("2") -> "Niederösterreich"
-    this.startsWith("3") -> "Niederösterreich"
-    this.startsWith("4") -> "Oberösterreich"
-    this.startsWith("5") -> "Salzburg"
-    this.startsWith("6") -> "Tirol"
-    this.startsWith("7") -> "Burgenland"
-    this.startsWith("8") -> "Steiermark"
-    this.startsWith("9") -> "Kärnten"
-    else -> "Österreich"
-}
-
-private val randomEyeColour
-    get() = listOf(
-        "black",
-        "blue",
-        "brown",
-        "dichromatic",
-        "grey",
-        "green",
-        "hazel",
-        "maroon",
-        "pink",
-        "unknown"
-    ).random()
-
-private val randomHairColour
-    get() = listOf(
-        "bald",
-        "black",
-        "blond",
-        "brown",
-        "grey",
-        "red",
-        "auburn",
-        "sandy",
-        "white",
-        "unknown"
-    ).random()
-
-data class Address(val postCode: String, val city: String, val state: String, val street: String, val locator: Int)
-
-private val randomAddress: Address
-    get() = listOf(
-        Address("6900", "Bregenz", "Vorarlberg", randomStreet, randomAddressLocator),
-        Address("6010", "Innsbruck", "Tirol", randomStreet, randomAddressLocator),
-        Address("5010", "Salzburg", "Salzburg", randomStreet, randomAddressLocator),
-        Address("4020", "Linz", "Oberösterreich", randomStreet, randomAddressLocator),
-        Address("3100", "St. Pölten", "Niederösterreich", randomStreet, randomAddressLocator),
-        Address("1010", "Wien", "Wien", randomStreet, randomAddressLocator),
-        Address("8010", "Graz", "Steiermark", randomStreet, randomAddressLocator),
-        Address("7000", "Eisenstadt", "Burgenland", randomStreet, randomAddressLocator),
-        Address("9020", "Klagenfurt", "Kärnten", randomStreet, randomAddressLocator)
-    ).random()
-
-private val randomAddressLocator
-    get() = Random.nextInt(1, 99)
-
-private val randomStreet
-    get() = listOf("Hauptstraße", "Herrengasse", "Hauptplatz", "Landstraße", "Dorfstraße").random()
-
-private val randomDateOfBirth
-    get() = LocalDate(Random.nextInt(1970, 2000), Random.nextInt(1, 12), Random.nextInt(1, 28))
-
-private val pictureTripleX
-    get() = """
-    /9j/4AAQSkZJRgABAQEBLAEsAAD//gATQ3JlYXRlZCB3aXRoIEdJTVD/4gKwSUNDX1BST0ZJTEUA
-    AQEAAAKgbGNtcwRAAABtbnRyUkdCIFhZWiAH6QAEAAcABgAbAAphY3NwQVBQTAAAAAAAAAAAAAAA
-    AAAAAAAAAAAAAAAAAAAA9tYAAQAAAADTLWxjbXMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    AAAAAAAAAAAAAAAAAAAAAAAAAA1kZXNjAAABIAAAAEBjcHJ0AAABYAAAADZ3dHB0AAABmAAAABRj
-    aGFkAAABrAAAACxyWFlaAAAB2AAAABRiWFlaAAAB7AAAABRnWFlaAAACAAAAABRyVFJDAAACFAAA
-    ACBnVFJDAAACFAAAACBiVFJDAAACFAAAACBjaHJtAAACNAAAACRkbW5kAAACWAAAACRkbWRkAAAC
-    fAAAACRtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACQAAAAcAEcASQBNAFAAIABiAHUAaQBsAHQALQBp
-    AG4AIABzAFIARwBCbWx1YwAAAAAAAAABAAAADGVuVVMAAAAaAAAAHABQAHUAYgBsAGkAYwAgAEQA
-    bwBtAGEAaQBuAABYWVogAAAAAAAA9tYAAQAAAADTLXNmMzIAAAAAAAEMQgAABd7///MlAAAHkwAA
-    /ZD///uh///9ogAAA9wAAMBuWFlaIAAAAAAAAG+gAAA49QAAA5BYWVogAAAAAAAAJJ8AAA+EAAC2
-    xFhZWiAAAAAAAABilwAAt4cAABjZcGFyYQAAAAAAAwAAAAJmZgAA8qcAAA1ZAAAT0AAACltjaHJt
-    AAAAAAADAAAAAKPXAABUfAAATM0AAJmaAAAmZwAAD1xtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgA
-    AAAcAEcASQBNAFBtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEL/2wBDAAMCAgMC
-    AgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIU
-    FRT/2wBDAQMEBAUEBQkFBQkUDQsNFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQU
-    FBQUFBQUFBQUFBQUFBT/wgARCAAhAGEDAREAAhEBAxEB/8QAHAAAAQUBAQEAAAAAAAAAAAAACAME
-    BQYHAAIB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEAMQAAAB8G1lUI0aljJ4yM0MqRsg
-    M4YI3AxJMMM+AbDcM0VBMKqGWSQiBaTIX5wG42DOPYKBTg0h8CybAVETGpLk8ZaaKUY2QHEL0BAO
-    84AwUDvOAMJINMfAJhan/8QAIxAAAQQCAgICAwAAAAAAAAAABgMEBQcAAgEVEDcWNhQxNP/aAAgB
-    AQABBQK0xNRg6CCpMrhbOMfj0XVYZ0zG1gzsmtaGPySKOivQUhapEd1lbIDeCeLqcv5etyYgQGYi
-    vR1cunccIJukHOrqpzAMhXB+S5+8LIheuShig5tcvSS0QTyyxlUfk3Ui/tggjo9CKY4qroglJOHd
-    rFoJPOAkg8Gk24PiSGeOqsLNN9VNMs8pVmH/AOLI1MRMXqMi0y1S3d0uCiegpDWiG99G1aZ94wtU
-    z6hlWQb8djD4R1K4eqC/bjmxTHgXianD+W6RUOIlEPXJIsLzODXtLwD+yin2j4l/adx/bmP8OW59
-    zz//xAAUEQEAAAAAAAAAAAAAAAAAAABg/9oACAEDAQE/AQP/xAAUEQEAAAAAAAAAAAAAAAAAAABg
-    /9oACAECAQE/AQP/xAA7EAABAgIFBgoKAwEAAAAAAAACAQMABAUREjFBBhAhQlFxExQVIiMyUmGx
-    0SQlM0Nic3SBsvB1wcLh/9oACAEBAAY/Ahyio1FAkJFfsapYHAP6Emm+Y+CYFt3LHFJY6p+aSpKv
-    dhiUcpzYVTsyPMEk9mHmscryYelsJ0ojrht3pHF5g66QlUqP4xwKCdFUWcd5jALt2/aFyhpBFJw1
-    VWEPFcXI4aXH1hLJW38aYjHIs4XpUunQqWsHZ3pD08/ps6ADtlgkPZQUpz2hctJWmhxzyTM4y8CO
-    NODZICuVI4RtCeo5+4a+uGzen7fD9O0mNco2daCtxFgCdyZ2KYo0apJ09AJci6za92z/AJBTD4k3
-    RjF419UMB3rAttigAKVCKYJmaykovok4RCdsah9rcv7fElKACy0q0KKaItaB2y/pIZlJYODYaGyK
-    ZjccJAbBLREtyJCS8qqt0ezXZJU0AGJL3rD2T9KrYYNyoSW4DwXcWdihKLW3KNnVaS4ixNe5IORn
-    SU6OfqrNLlTBxN2MCYqhCSVoqY5gycoyt1baI9Y1zwD7RKvmvGJV4Et2LjTWHekNTMuaOMujaEkz
-    Dk9RyqZmSI/Y1lwbgWlRFnHee+abdm5I4/KhXPyo3ImlwNkcnTR+nSw80lXS4G3ekclSh1TkwPSE
-    Puw81jjcyFVITKaa/djgPnCiCIk8zzmD8R+8cgT6qLrdfF1O/vCLDJesJhFFr4NpRy5OjamH/YWr
-    0Fdbev7fDsk7UhdZpyrqHgsO5PUp0bZOWQtL7NzyX9vzB9a7/rPLfPd/Eoc+sa/znX+Rb/JIT6YP
-    FYl/lj4ZnvlB4Zv/xAAkEAEAAQMEAgIDAQAAAAAAAAABEQAhMUFRYYFxkRDwIKHxwf/aAAgBAQAB
-    PyHVcZQ233A8w6tSJhLNYw9h2aVmqqq+DyaHbpV8gGUe/WR4g3pma7uF9PHgrBYJ84vLo8+anETd
-    J1s2y9GtAo9W6N7luHbtVw1kec6/5z5acuE4iGV9Y8NIYQXd1w/cTRhMf8SDr9Bo/C5MGSohGoVz
-    cW/dc8XohItK09Tid7btYpAIkjo1POxVDvYEtERb/Xl35l87FAYMTAGA+MHENl236Ly00yITFLur
-    +jdolgR9jV3dZ+HTpRgF1aD7o41LdiDwaLTDJTLzDxR+ufhYp+mxyWZtTHeZKc5kJC8Te4HnNqJG
-    RaQOE+JpUHnJYdsnnw1NSMYi2yalx8bpQPWa8PwlynzVtdwvRvUOrbtOkvR7dazFBZQ7vNMnZrUL
-    OISQ7HLA9O9Ti7orr/DxPFDEx05ZfJr0NKmvSwy6jtD3DS53hMWZZ1Lx2aFJjQiZGr4057qQ4Sm/
-    KP6xSS1kiSX8Hhan2ZS1sX6+UdX4ZOPzWpq+P2Gz8Jv/2gAMAwEAAgADAAAAEAIBIBBJABBJBJIJ
-    AIJJIBAIIJBJBJIIIJ//xAAUEQEAAAAAAAAAAAAAAAAAAABg/9oACAEDAQE/EAP/xAAUEQEAAAAA
-    AAAAAAAAAAAAAABg/9oACAECAQE/EAP/xAAgEAEBAAICAwEBAQEAAAAAAAABESExAEEQUXFh8CCB
-    /9oACAEBAAE/EC4L+IiHoRD+DJxxR6IMMXo9k2VcCOy2E6Gln94deIWCFGIES4NNCQ5CW6D7OTvI
-    4y2ZhwphWY6K94Yeg4CcPY3Wj8DRdLkOLfkgOsnKin9zVcQhXknwErK3WFDhGjgWJKPgGFnGbiUD
-    XBz/ANiVeguuNG1GEkDcOIYtSB4FiStmGdiKcEJLdBQqcXQtiNBHGgPcYKTuh8DeYAAIGg4ZYUQo
-    nrh7XCDCgMAvQDBQ8O+2YgKhTAQZChw8A7dYYADQAAeB5Z0Q5wGKMdKW1I8jqmUoaAepxXi0xyqB
-    sco1LKqu/B9ITBFB0AKvEqsPRJXSkFRji4OixeSUv2elSHiAVQDKvXEIJAuW7ZEXZRhcLi4yqlFi
-    QFYHR8FmhIVUGEREfAR5mhYbEpF1AKZtL4SuwlB4qUrADT+PQ1PiaRyIjrwsUgqCIMqop2+rwMhd
-    SX6AqPawK4CAQdqhTK133EUTpjsMiFzgbVGisIwqTTGJdg0TJTCrkVAMr4G6WP22S8D2s6YHpDB6
-    BmkXWo2G2fIUR6CBBZumEwwT1cCNZhCErmpdyz7GO7W5Q/F0MBj3MoCUHfHVe7CkJcNKjEDCeP73
-    vx2fPC3+/wCv9sLW1/B9vH//2Q==
-""".trimIndent().decodeToByteArray(Base64())
 
 private const val EHIC_VCTM = """
     eyJ2Y3QiOiJ1cm46ZXVkaTplaGljOjEiLCJuYW1lIjoiRUhJQyBTRC1KV1QgVFlQRSBNRVRBREFUQSIs
@@ -842,3 +509,9 @@ private const val EHIC_VCTM = """
     LXNjaGVtYSIsInNjaGVtYV91cmkjaW50ZWdyaXR5Ijoic2hhMjU2LWNOUzJhalByNnBmWnp0RTBLNVlL
     NGg3RWlTNzNQQ2oxL3YvM1ZmMXpkMUU9In0
 """
+
+private fun formatAddress(street: String, locator: Int, postalCode: String, city: String) =
+    "$street $locator, $postalCode $city"
+
+private fun claim(key: String, useSd: Boolean, value: () -> Any?): ClaimToBeIssued? =
+    value()?.let { ClaimToBeIssued(key, it, useSd) }
---
+++ http/src/main/kotlin/at/asitplus/wallet/backend/config/IdAustriaDataExtractor.kt
@@ -0,0 +1,106 @@
+package at.asitplus.wallet.backend.config
+
+import at.asitplus.openid.OidcUserInfoExtended
+import at.asitplus.wallet.eupid.IsoIec5218Gender
+import at.asitplus.wallet.mdl.IsoSexEnum
+import io.matthewnelson.encoding.base64.Base64
+import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
+import kotlinx.datetime.LocalDate
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import java.nio.charset.Charset
+import kotlin.random.Random
+
+fun OidcUserInfoExtended.parseIdAustriaAddress(): Address? =
+    getClaimAsString("urn:eidgvat:attributes.mainAddress")?.let { idaAddress ->
+        runCatching {
+            idaAddress.parseIdAustriaAddress()
+        }.getOrNull()
+    }
+
+private fun String.parseIdAustriaAddress(): Address? {
+    val json = Json.parseToJsonElement(
+        decodeToByteArray(Base64()).toString(Charset.defaultCharset())
+    ) as? JsonObject
+    val postCode = json.getPrimitiveContent("Postleitzahl")
+    val city = json.getPrimitiveContent("Ortschaft")
+    val street = json.getPrimitiveContent("Strasse")
+    val locator = json.getPrimitiveContent("Hausnummer")
+    return if (postCode != null && city != null && street != null && locator != null) {
+        Address(
+            postCode = postCode,
+            city = city,
+            state = postCode.toState(),
+            street = street,
+            locator = locator.toIntOrNull() ?: Random.nextInt(1, 99)
+        )
+    } else {
+        null
+    }
+}
+
+private fun JsonObject?.getPrimitiveContent(key: String) = (this?.get(key) as? JsonPrimitive)?.content
+
+val OidcUserInfoExtended.sex
+    get() = getClaimAsString("urn:eidgvat:attributes.gender")?.toIsoSexEnum()
+        ?: IsoSexEnum.NOT_KNOWN
+
+val OidcUserInfoExtended.gender
+    get() = getClaimAsString("urn:eidgvat:attributes.gender")?.toIsoGenderEnum()
+        ?: IsoIec5218Gender.NOT_KNOWN
+
+fun String.toIsoSexEnum() = when (this) {
+    "W" -> IsoSexEnum.FEMALE
+    "M" -> IsoSexEnum.MALE
+    else -> IsoSexEnum.NOT_KNOWN
+}
+
+fun String.toIsoGenderEnum() = when (this) {
+    "W" -> IsoIec5218Gender.FEMALE
+    "M" -> IsoIec5218Gender.MALE
+    else -> IsoIec5218Gender.NOT_KNOWN
+}
+
+val OidcUserInfoExtended.arrivalDate: LocalDate
+    get() = getClaimAsString("urn:eidgvat:attributes.mainAddressRegistrationDate")
+        ?.let { runCatching { LocalDate.parse(it) }.getOrNull() }
+        ?: LocalDate(2000, Random.nextInt(1, 12), Random.nextInt(1, 28))
+
+val OidcUserInfoExtended.nationality: String
+    get() = getClaimAsString("urn:eidgvat:attributes.nationality")?.let {
+        runCatching {
+            Json.parseToJsonElement(it).jsonArray.first().jsonPrimitive.content
+        }.getOrNull()
+            ?.mapToAlpha2()
+    } ?: "AT"
+
+fun String.mapToAlpha2() = when (this) {
+    "AUT" -> "AT"
+    "DEU" -> "DE"
+    "CHE" -> "CH"
+    else -> "XX"
+}
+
+val OidcUserInfoExtended.legalName: String
+    get() = getClaimAsString("urn:pvpgvat:oidc.mandator_legal_person_full_name")
+        ?: "A-SIT Plus GmbH"
+
+val OidcUserInfoExtended.legalPersonIdentifier: String
+    get() = getClaimAsString("urn:pvpgvat:oidc.mandator_legal_person_source_pin")
+        ?: "XFN+436920f"
+
+private fun String.toState(): String = when {
+    this.startsWith("1") -> "Wien"
+    this.startsWith("2") -> "Niederösterreich"
+    this.startsWith("3") -> "Niederösterreich"
+    this.startsWith("4") -> "Oberösterreich"
+    this.startsWith("5") -> "Salzburg"
+    this.startsWith("6") -> "Tirol"
+    this.startsWith("7") -> "Burgenland"
+    this.startsWith("8") -> "Steiermark"
+    this.startsWith("9") -> "Kärnten"
+    else -> "Österreich"
+}
