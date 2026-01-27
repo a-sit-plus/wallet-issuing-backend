4a9540e36ce6b4b71e3a9db35f1297264baa0873
Christian Kollmann <christian.kollmann@a-sit.at>
2026-01-27 09:57:17 +0100

    Extract controller for OAuth2

--- http/src/main/kotlin/at/asitplus/wallet/backend/controller/OpenId4VciController.kt
+++ http/src/main/kotlin/at/asitplus/wallet/backend/controller/OpenId4VciController.kt
@@ -1,17 +1,12 @@
 package at.asitplus.wallet.backend.controller
 
-import at.asitplus.catching
 import at.asitplus.openid.DisplayLogoProperties
 import at.asitplus.openid.DisplayProperties
 import at.asitplus.openid.IssuerMetadata
 import at.asitplus.openid.JwtVcIssuerMetadata
-import at.asitplus.openid.OAuth2AuthorizationServerMetadata
 import at.asitplus.openid.OpenIdConstants
 import at.asitplus.openid.OpenIdConstants.Errors
-import at.asitplus.openid.RequestParameters
-import at.asitplus.openid.TokenRequestParameters
 import at.asitplus.wallet.backend.Paths
-import at.asitplus.wallet.backend.auth.SpringSecurityAuthenticationSupplier
 import at.asitplus.wallet.backend.config.BackendConfigurationProperties
 import at.asitplus.wallet.backend.config.MetadataConfiguration
 import at.asitplus.wallet.backend.data.OidcIssuerCredentialDataProvider
@@ -22,13 +17,10 @@ import at.asitplus.wallet.lib.ktor.openid.DPoPNonce
 import at.asitplus.wallet.lib.ktor.openid.OAuthClientAttestation
 import at.asitplus.wallet.lib.ktor.openid.OAuthClientAttestationPop
 import at.asitplus.wallet.lib.oauth2.RequestInfo
-import at.asitplus.wallet.lib.oauth2.SimpleAuthorizationService
 import at.asitplus.wallet.lib.oidvci.CredentialIssuer
 import at.asitplus.wallet.lib.oidvci.OAuth2Error
 import at.asitplus.wallet.lib.oidvci.OAuth2Exception
 import at.asitplus.wallet.lib.oidvci.WalletService
-import at.asitplus.wallet.lib.oidvci.decodeFromPostBody
-import at.asitplus.wallet.lib.oidvci.decodeFromUrlQuery
 import io.github.aakira.napier.Napier
 import io.ktor.client.utils.CacheControl
 import io.ktor.http.*
@@ -37,26 +29,18 @@ import kotlinx.coroutines.runBlocking
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.ModelAndView
 
 
 /**
- * Implements controller for OpenID 4 Verifiable Credential Issuance
+ * Implements controller for OpenID4VCI, mapping the public functions of [CredentialIssuer].
  */
 @RestController
 class OpenId4VciController(
     private val credentialIssuer: CredentialIssuer,
-    private val authorizationService: SimpleAuthorizationService,
     private val backendConfigurationProperties: BackendConfigurationProperties,
 ) {
 
@@ -77,20 +61,6 @@ class OpenId4VciController(
         logo = DisplayLogoProperties(uri = logo)
     )
 
-    @GetMapping(OpenIdConstants.PATH_WELL_KNOWN_OPENID_CONFIGURATION, produces = [APPLICATION_JSON_VALUE])
-    fun openidMetadata(): ResponseEntity<OAuth2AuthorizationServerMetadata> = runBlocking {
-        val metadata = authorizationService.metadata()
-        Napier.i("${OpenIdConstants.PATH_WELL_KNOWN_OPENID_CONFIGURATION} returns $metadata")
-        ResponseEntity.ok(metadata)
-    }
-
-    @GetMapping(OpenIdConstants.PATH_WELL_KNOWN_OAUTH_AUTHORIZATION_SERVER, produces = [APPLICATION_JSON_VALUE])
-    fun oauthMetadata(): ResponseEntity<OAuth2AuthorizationServerMetadata> = runBlocking {
-        val metadata = authorizationService.metadata()
-        Napier.i("${OpenIdConstants.PATH_WELL_KNOWN_OAUTH_AUTHORIZATION_SERVER} returns $metadata")
-        ResponseEntity.ok(metadata)
-    }
-
     @GetMapping(
         value = [OpenIdConstants.PATH_WELL_KNOWN_JWT_VC_ISSUER_METADATA,
             OpenIdConstants.PATH_WELL_KNOWN_JWT_VC_ISSUER_METADATA + "/*"
@@ -101,34 +71,6 @@ class OpenId4VciController(
         val metadata = credentialIssuer.jwtVcMetadata
         Napier.i("${OpenIdConstants.PATH_WELL_KNOWN_JWT_VC_ISSUER_METADATA} returns $metadata")
         ResponseEntity.ok(metadata)
-    }
-
-    /**
-     * Called by the Wallet when pushing an authorization request, see [SimpleAuthorizationService.par]
-     */
-    @PostMapping(Paths.ParUrl, produces = [APPLICATION_JSON_VALUE])
-    fun par(
-        @RequestBody requestBody: String,
-        request: HttpServletRequest,
-    ): ResponseEntity<*> = runBlocking {
-        Napier.i("${Paths.ParUrl} called")
-        Napier.v("${Paths.ParUrl} called with $requestBody")
-        val params: RequestParameters = requestBody.decodeFromPostBody()
-            ?: return@runBlocking buildOidcErrorResponse(OAuth2Exception.InvalidRequest())
-        val result = authorizationService.parWithDpopNonce(
-            request = params,
-            httpRequest = request.toRequestInfo().also {
-                Napier.v("${Paths.ParUrl} called with $it")
-            },
-        ).getOrElse {
-            Napier.w("${Paths.ParUrl} got error", it)
-            return@runBlocking buildOidcErrorResponse(it)
-        }
-        Napier.d("${Paths.ParUrl} returns $result")
-        ResponseEntity.status(HttpStatus.CREATED)
-            .header(HttpHeaders.CacheControl, CacheControl.NO_STORE)
-            .header(HttpHeaders.DPoPNonce, result.dpopNonce)
-            .body(vckJsonSerializer.encodeToString(result.response))
     }
 
     /**
@@ -143,78 +85,6 @@ class OpenId4VciController(
             return@runBlocking buildOidcErrorResponse(it)
         }
         Napier.d("${Paths.NonceUrl} returns $result")
-        ResponseEntity.status(HttpStatus.OK)
-            .header(HttpHeaders.CacheControl, CacheControl.NO_STORE)
-            .header(HttpHeaders.DPoPNonce, result.dpopNonce)
-            .body(vckJsonSerializer.encodeToString(result.response))
-    }
-
-    /**
-     * Logs out the user from the Spring Boot session, so that new requests need to be authorized again,
-     * using the configured OAuth2 AS. Subsequent requests to [token] and [credential] are secured
-     * by the authorization code returned here.
-     * See [SimpleAuthorizationService.authorize].
-     */
-    // TODO add "PreAuthorize" annotation?
-    @RequestMapping(Paths.AuthorizeUrl, method = [RequestMethod.POST, RequestMethod.GET])
-    fun authorize(
-        @RequestParam requestParams: Map<String, String>,
-        @RequestBody requestBody: String?,
-        request: HttpServletRequest,
-        model: ModelMap,
-        authentication: Authentication? = null,
-    ) = runBlocking {
-        Napier.i("${Paths.AuthorizeUrl} called")
-        Napier.v("${Paths.AuthorizeUrl} called with $requestParams and $requestBody")
-        val params: RequestParameters =
-            if (requestBody.isNullOrEmpty()) requestParams.decodeFromUrlQuery()
-            else requestBody.decodeFromPostBody()
-
-        val result = authorizationService.authorize(params) {
-            catching {
-                SpringSecurityAuthenticationSupplier.toOidcUserInfoExtended(authentication)
-                    ?: SecurityContextHolder.getContext().authentication
-                        ?.let { SpringSecurityAuthenticationSupplier.toOidcUserInfoExtended(it) }
-                    ?: throw IllegalArgumentException("No authenticated user")
-            }
-        }.getOrElse {
-            Napier.w("${Paths.AuthorizeUrl} got error", it)
-            return@runBlocking buildOidcErrorResponse(it)
-        }
-        Napier.d("${Paths.AuthorizeUrl} returns ${result.url}")
-        val userAgent = request.getHeader(HttpHeaders.UserAgent)
-        if (userAgent?.isSafariOniPhone() == true) {
-            model["url"] = result.url
-            ModelAndView("iphone-redirect")
-        } else {
-            buildOidcRedirect(result.url)
-        }.also { request.logout() }
-    }
-
-    /** Display a manual redirect back into the Wallet app, but only for iPhones */
-    private fun String.isSafariOniPhone() = contains("Safari") && contains("iPhone")
-
-    /**
-     * Handles the token request sent by Wallets, see [SimpleAuthorizationService.tokenWithDpopNonce].
-     */
-    @PostMapping(Paths.TokenUrl, produces = [APPLICATION_JSON_VALUE])
-    fun token(
-        @RequestBody requestBody: String,
-        request: HttpServletRequest,
-    ): ResponseEntity<*> = runBlocking {
-        Napier.i("${Paths.TokenUrl} called")
-        Napier.v("${Paths.TokenUrl} called with $requestBody")
-        val params: TokenRequestParameters = requestBody.decodeFromPostBody()
-            ?: return@runBlocking buildOidcErrorResponse(OAuth2Exception.InvalidRequest())
-        val result = authorizationService.tokenWithDpopNonce(
-            request = params,
-            httpRequest = request.toRequestInfo()
-                .also { Napier.v("${Paths.TokenUrl} called with $it") }
-        ).getOrElse {
-            Napier.w("${Paths.TokenUrl} got error", it)
-            return@runBlocking buildOidcErrorResponse(it)
-        }
-        Napier.d("${Paths.TokenUrl} returns $result")
         ResponseEntity.status(HttpStatus.OK)
             .header(HttpHeaders.CacheControl, CacheControl.NO_STORE)
             .header(HttpHeaders.DPoPNonce, result.dpopNonce)
@@ -270,21 +140,14 @@ class OpenId4VciController(
 
     private suspend fun CredentialIssuer.CredentialResponse.Plain.toResponseEntity(): ResponseEntity<String?> =
         ResponseEntity.status(HttpStatus.OK)
-            .header(HttpHeaders.DPoPNonce, authorizationService.getDpopNonce())
             .header(HttpHeaders.ContentType, MediaTypes.Application.JSON)
             .body(vckJsonSerializer.encodeToString(response))
 
     private suspend fun CredentialIssuer.CredentialResponse.Encrypted.toResponseEntity(): ResponseEntity<String?> =
         ResponseEntity
             .status(HttpStatus.OK)
-            .header(HttpHeaders.DPoPNonce, authorizationService.getDpopNonce())
             .header(HttpHeaders.ContentType, MediaTypes.Application.JWT)
             .body(vckJsonSerializer.encodeToString(response.serialize()))
-
-    private fun buildOidcRedirect(location: String): ResponseEntity<String> = ResponseEntity
-        .status(HttpStatus.FOUND)
-        .header(HttpHeaders.Location, location)
-        .build()
 
     private fun buildOidcErrorResponse(throwable: Throwable): ResponseEntity<OAuth2Error> =
         when (throwable) {
